6c1791447672d5acb84f2bdcc5d8f24f8049940c
skysrd <skysrd@gmail.com>
2022-08-02 16:27:03 +0900

    [PATCH] AuthService

---
+++ src/main/java/com/skysrd/raidweeklyplanner/service/ContentService.java
@@ -0,0 +1,12 @@
+package com.skysrd.raidweeklyplanner.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ContentService {
+
+}
